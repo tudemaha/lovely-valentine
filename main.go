9a7e7828705c1282e3c9d39ad17dc14a5db89df0
tudemaha <gede_mahardika88@yahoo.co.id>
2024-02-10 17:27:33 +0800

    finish all view

--- main.go
+++ main.go
@@ -10,6 +10,24 @@ func main() {
 	fs := http.FileServer(http.Dir("./public"))
 	http.Handle("/public/", http.StripPrefix("/public/", fs))
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.ParseFiles("./view/index.html")
+		if err != nil {
+			fmt.Println("error: ", err)
+			return
+		}
+
+		data := struct {
+			QuotesNum []uint8
+			Quotes    []string
+		}{
+			QuotesNum: []uint8{0, 1, 2, 3, 4},
+			Quotes:    []string{"hai", "ini", "test", "dinisi", "testlagi"},
+		}
+
+		t.Execute(w, data)
+	})
+
 	http.HandleFunc("/create", func(w http.ResponseWriter, r *http.Request) {
 		t, err := template.ParseFiles("./view/create.html")
 		if err != nil {
@@ -36,7 +54,7 @@ func main() {
 		data := struct {
 			Status bool
 		}{
-			Status: false,
+			Status: true,
 		}
 
 		t.Execute(w, data)
